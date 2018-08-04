fd8eeeb9c6d22c560c0e3df196ed27a61d9edefc
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-08-04 02:06:14 +0000

    add clean-docker-registry.sh

---
+++ clean-docker-registry.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# scan registry and delete folders with no sub-folders in
+
+# Use the Unofficial Bash Strict Mode
+set -o errexit
+set -o nounset
+set -o pipefail
+IFS=$'\n\t'
+
+set +o verbose
+set +o xtrace
+export TERM=linux
+
+
+function removeEmptyTags()
+{
+    local -r dir=${1:?}
+    
+    local -a dirs
+    echo -n " checking $(basename "$dir")"
+    mapfile -t dirs < <(ls -1A "${dir}/_manifests/tags")
+    if [ ${#dirs[*]} -eq 0 ]; then
+        rm -rf "$dir"
+        echo -n ': deleted'
+    fi
+    echo
+}
+
+# ensure this script is run as root
+if [[ $EUID != 0 ]]; then
+  sudo $0
+  exit
+fi
+
+echo
+echo 
+declare -i status
+for dir in $(find /var/lib/docker-registry/docker/registry/v2/repositories/ -mindepth 1 -maxdepth 1 -type d -name '*' | sort); do
+    removeEmptyTags "$dir"
+done
+echo 
+df
