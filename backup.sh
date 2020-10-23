dc79452a15fa226530bb908dace173dc08e667b2
Ballantyne, Bob <ballantyne.robert@gmail.com>
2020-10-23 08:49:27 -0400

    implement backup of important files

---
+++ backup.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+source /home/bobb/.bin/trap.bashlib
+
+#---------------------------------------------------------------------------- 
+function main() {
+
+    local -r target="${1:?}"
+
+    local -r filename="$(basename "$target")"
+    if [ -e "$filename" ]; then
+        [ "$(sha256sum "$target")" = "$(sha256sum "${BACKUP_DIR}/$filename")" ] && return 0
+    fi
+
+    local -r base="${filename%.*}" 
+    sudo mkdir -p "${BACKUP_DIR}/$base"
+
+    local newfile="${BACKUP_DIR}/${base}/${base}.$(date +"%Y%m%d").${filename##*.}"
+    sudo cp "$target" "$newfile"
+    updateStatus "addBadge('completed.gif','${NODENAME}: backed up to $newFile')"
+
+    [ -e "${BACKUP_DIR}/$filename" ] && sudo rm "${BACKUP_DIR}/$filename"
+    sudo ln -s "$newfile" "${BACKUP_DIR}/$filename"
+
+    local -a files
+    mapfile -t files < <(find "${BACKUP_DIR}/$base" -maxdepth 1 -mindepth 1 -type f | sort -r)
+
+    local -i i="${#files[*]}"
+    while [ $(( i-- )) -ge "$MAX_FILES" ]; do
+        sudo rm "${files[$i]}"
+        updateStatus "addBadge('completed.gif','${NODENAME}: backed up to $newFile\\nremoved ${files[$i]}')" 'force'
+    done
+    return 0
+}
+
+#----------------------------------------------------------------------------
+function onexit()
+{
+    echo
+    return 1
+}
+
+#----------------------------------------------------------------------------
+function updateStatus()
+{
+    local -r text=${1:?}
+    local -r force=${2:-}
+
+    [ -z "${text:-}" ] && return 0
+    if [ "${force:-}" ] || [ ! -s "$JOB_STATUS" ]; then
+        (echo "manager.$text"; echo "currentBuild.result = 'UNSTABLE'") > "$JOB_STATUS"
+    fi
+    return 0
+}
+ 
+##########################################################################################################
+
+set -o errtrace
+
+declare -r MAX_FILES=10
+declare -r BACKUP_DIR=/home/bobb/src
+
+export TERM=linux
+declare -r NODENAME=$(hostname -f)
+export RESULTS="./${NODENAME}.txt" 
+export JOB_STATUS=./status.groovy 
+
+trap onexit ERR
+trap onexit INT
+trap onexit PIPE
+trap onexit EXIT
+
+main "$@" 2>&1 | tee "$RESULTS"
