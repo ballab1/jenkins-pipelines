a68f7f24d973c1466650a75d4cab873a1359532a
Ballantyne, Bob <ballantyne.robert@gmail.com>
2021-01-05 20:19:03 -0500

    include directory info

--- scanShareFiles.py
+++ scanShareFiles.py
@@ -26,16 +26,20 @@ import pwd
 import random
 import re
 import socket
-import stat
+#import stat
 import sys
 import tempfile
 import traceback
+from stat import *
 from encodings.aliases import aliases
 #from PyJsonFriendly import JsonFriendly
 from uuid import uuid1
 
 # 3rd party imports
 #from confluent_kafka import Producer, Consumer, KafkaError, OFFSET_BEGINNING, OFFSET_END, OFFSET_STORED, OFFSET_INVALID
+
+
+BLKSIZE = 131072
 
 
 #-----------------------------------------------------------------------------
@@ -53,109 +57,78 @@ Usage:
 
 
 #-----------------------------------------------------------------------------
+def fileInfo(file, data):
 
-MOUNTS = [ "Guest/All Users.backups_300/Music",
-           "Guest/All Users/Music.todo/20100619.Music.org",
-           "Guest/All Users/Music.todo/20100702/Music",
-           "Guest/All Users/Music.todo/20100702/Music.ToDo",
-           "Guest/All Users/Music.todo/20100717/Music.Partial",
-           "Guest/All Users/Music.todo/20100717/Music.ToDo",
-           "Guest/All Users/Music.todo/20100822/Music",
-           "Guest/All Users/Music.todo/20100822/Music.Partial",
-           "Guest/All Users/Music.todo/20100822/Music.ToDo",
-           "Guest/All Users/Music.todo/Music.300gb-DRIVE",
-           "Guest/All Users/Music.todo",
-           "Guest/All Users",
-           "Public/Shared Music"
-         ]
+    data['file'] = file
+
+    data['name'] = os.path.basename(file)
+    data['folder'] = os.path.dirname(file)
+    data['depth'] = file.count('/')
 
 
-DIRS = [ "Ace Of Base/The Bridge",
-         "Bright Eyes/Digital Ash In A Digital Urn",
-         "Eagles/The Long Run",
-         "Faust/Faust IV",
-         "Frank Zappa/Chunga's Revenge",
-         "Franz Ferdinand/Franz Ferdinand",
-         "JO",
-         "Jean-Michel Jarre",
-         "Jethro Tull/Living In The Past (disc 1)",
-         "Level 42/True Colours",
-         "OutKast/Speakerboxxx-The Love Below Disc 2",
-         "Sansa/Albums",
-         "Santana/Zebop!",
-         "Spyro Gyra/Carnaval",
-         "Style Council",
-         "Vangelis",
-         "Wendy Carlos/Switched On Bach II"
-        ]
+    for key in ['dir_count', 'file_count', 'size']:
+        if key in data.keys():
+            del data[key]
 
 
-MODES = { stat.S_IFSOCK : 'socket',
-          stat.S_IFLNK  : 'symlink',
-          stat.S_IFREG  : 'regular file',
-          stat.S_IFBLK  : 'block device',
-          stat.S_IFDIR  : 'directory',
-          stat.S_IFCHR  : 'character device',
-          stat.S_IFIFO  : 'FIFO/pipe'
-        }
+    try:
+        fstat = os.stat(file)
+        data['blocks'] = fstat.st_blocks
+        data['block_size'] = fstat.st_blksize
+        data['device'] = fstat.st_dev
+        data['device_type'] = fstat.st_rdev
+        data['gid'] = fstat.st_gid
+        if groupName(fstat.st_gid):
+            data['gname'] = groupName(fstat.st_gid)
+        elif 'gname' in data.keys():
+            del data['gname']
+        data['hard_links'] = fstat.st_nlink
+        data['inode'] = fstat.st_ino
+        data['last_access'] = int(fstat.st_atime)
+        data['last_access_time'] = zulu_timestamp(fstat.st_atime)
+        data['last_modified'] = int(fstat.st_mtime)
+        data['last_modified_time'] = zulu_timestamp(fstat.st_mtime)
+        data['last_status_change'] = int(fstat.st_ctime)
+        data['last_status_change_time'] = zulu_timestamp(fstat.st_ctime)
+        data['mode_8'] = oct(fstat.st_mode)
+#        data['mode_x'] = stat.filemode(fstat.st_mode)
+        data['size'] = fstat.st_size
+        data['type'] = fileType(fstat.st_mode)
+        data['uid'] = fstat.st_uid
+        if userName(fstat.st_uid):
+            data['uname'] = userName(fstat.st_uid)
+        elif 'uname' in data.keys():
+            del data['uname']
 
-BLKSIZE = 131072
-
-#-----------------------------------------------------------------------------
-def dirInfo(name, path, data):
-    data['name'] = to_unicode_or_bust(name)
-    data['folder'] = to_unicode_or_bust(path)
-
-#-----------------------------------------------------------------------------
-def fileInfo(name, path, data):
-    data['name'] = to_unicode_or_bust(name)
-    data['folder'] = to_unicode_or_bust(path)
-
-    file = os.path.join(path, name)
-
-    if os.path.islink(file):
-        for key in data.keys():
-            if key not in ['name', 'folder']:
-                del data[key]
-
-        if os.path.exists(file):
-            data['symlink_reference'] = os.path.realpath(file)
-        else:
-            data['link_reference'] = os.path.realpath(file)
-        return data
-    else:
-        for key in ['symlink_reference', 'link_reference']:
+    except:
+        for key in ['blocks', 'block_size', 'device', 'device_type', 'gid', 'gname', 'hard_links', 'inode', 'last_access', 'last_access_time', 'last_modified', 'last_modified_time', 'last_status_change', 'last_status_change_time', 'mode_8', 'mode_x', 'size', 'type', 'uid', 'uname']:
             if key in data.keys():
                 del data[key]
 
-    stat = os.stat(file)
-    data['size'] = stat.st_size
-    data['blocks'] = stat.st_blocks
-    data['block_size'] = stat.st_blksize
-    data['uid'] = stat.st_uid
-    data['uname'] = userName(stat.st_uid)
-    data['gid'] = stat.st_gid
-    data['gname'] = groupName(stat.st_gid)
-    data['access_rights'] = oct(stat.st_mode)
-    data['inode'] = stat.st_ino
-    data['hard_links'] = stat.st_nlink
-    data['st_dev'] = stat.st_rdev
-    data['file_type'] = fileType(stat.st_rdev)
-    data['last_access'] = int(stat.st_atime)
-    data['last_access_time'] = zulu_timestamp(stat.st_atime)
-    data['last_modified'] = int(stat.st_mtime)
-    data['last_modified_time'] = zulu_timestamp(stat.st_mtime)
-    data['last_status_change'] = int(stat.st_ctime)
-    data['last_status_change_time'] = zulu_timestamp(stat.st_ctime)
 
-    if os.path.isfile(file) and not os.path.islink(file):
-        data['sha256'] = sha256(file, stat.st_size)
-    elif 'sha256' in data.keys():
-        del data['sha256']
+    if os.path.islink(file):
+        if os.path.exists(file):
+            data['reference'] = os.path.realpath(file)
+            data['isvalid'] = 'true'
+        else:
+            data['reference'] = os.path.realpath(file)
+            data['isvalid'] = 'false'
+        return data
+
+
+    for key in ['reference', 'isvalid']:
+        if key in data.keys():
+            del data[key]
+
+
+    if not os.path.isdir(file):
+        data['sha256'] = sha256(file, fstat.st_size)
+
+
     return data
 
 
-def fileInfo_toadd(name, path, data):
+#def fileInfo_toadd(name, path, data):
 #    data['mount_point'] = path
 #    data['mount_source'] = path
 
@@ -165,16 +138,16 @@ def fileInfo_toadd(name, path, data):
 #    eval "stat_vals=( $(stat --format="['mount_point']='%m' ['time_of_birth']='%w'" "$1") )"
 #    local mount_source="$(grep -E '\s'"${stat_vals['mount_point']}"'\s' /etc/fstab | awk '{print $1}')"
 
-#    data['xfr_size_hint'] = stat.st_size
-#    data['device_number'] = stat.st_size
+#    data['xfr_size_hint'] = fstat.st_size
+#    data['device_number'] = fstat.st_size
 
-#    data['access_rights_time'] = stat.st_size
-#    data['raw_mode'] = stat.st_size
-#    data['device_type'] = stat.st_size
+#    data['access_rights_time'] = fstat.st_size
+#    data['raw_mode'] = fstat.st_size
+#    data['device_type'] = fstat.st_size
 
-#    data['file_created'] = stat.st_birthtime
-#    data['file_created_time'] = zulu_timestamp(stat.birthtime)
-    return data
+#    data['file_created'] = fstat.st_birthtime
+#    data['file_created_time'] = zulu_timestamp(fstat.birthtime)
+#    return data
 
 
 #-----------------------------------------------------------------------------
@@ -195,52 +168,36 @@ def userName(uid):
 
 #-----------------------------------------------------------------------------
 def fileType(mode):
-    mode = stat.S_IFMT(mode)
-    if mode in MODES.keys():
-        return MODES[mode]
-    return 'unknown'
+    if S_ISBLK(mode):
+        return "block device"
+    if S_ISCHR(mode):
+        return "character device"
+    if S_ISDIR(mode):
+        return "directory"
+    if S_ISFIFO(mode):
+        return "FIFO/pipe"
+    if S_ISLNK(mode):
+        return "symlink"
+    if S_ISREG(mode):
+        return "regular file"
+    if S_ISSOCK(mode):
+        return "socket"
+    return "unknown"
 
 
 #-----------------------------------------------------------------------------
 def sha256(file, size):
     if size > 0:
+        sha = hashlib.sha256()
         if size > BLKSIZE:
             size = BLKSIZE
         with open(file, mode='rb') as fd:
-            sha = hashlib.sha256()
             bytes = fd.read(size)
             while bytes != "":
                 sha.update(bytes)
                 bytes = fd.read(size)
-            return sha.hexdigest()
+        return sha.hexdigest()
     return ''
-
-
-#-----------------------------------------------------------------------------
-def to_unicode_or_bust(s, extended = False):
-    if all(ord(c) < 128 for c in s):
-        return s
-    if isinstance(s, unicode):
-        return s
-    try:
-        return unicode(s, 'windows_1250')
-    except:
-       pass
-
-    if extended:
-        keys = ['windows_1251', 'windows_1252', 'windows_1253', 'windows_1254', 'windows_1256', 'windows_1257', 'windows_1258', '1250', '1251', '1252', '1253', '1254', '1255', '1256', '1257', '1258', 'iso8859', 'iso8859_1', 'iso_8859_1', 'iso_8859_13', 'iso_8859_15', 'iso_8859_16', 'iso_8859_16_2001', 'iso_8859_1_1987', 'iso_8859_7', 'iso_8859_7_1987', 'iso_8859_8', 'iso_8859_8_1988', 'iso_8859_9', 'iso_8859_9_1989', 'iso_ir_100', 'iso_ir_126', 'iso_ir_138', 'iso_ir_148', 'l1', 'l10', 'l5', 'l5', 'l7', 'l9', 'latin', 'latin1', 'latin1', 'latin10', 'latin5', 'latin7', 'latin9', 'cp154', 'cp819', 'csisolatin1', 'csisolatin5', 'csisolatingreek', 'csisolatinhebrew', 'csptcp154', 'cyrillic_asian', 'ecma_118', 'elot_928', 'greek', 'greek8', 'hebrew', 'ibm819', 'pt154']
-        for t in keys:
-           try:
-               x =  unicode(s, t)
-               print'{} '.format(t),
-               print x
-           except:
-               pass
-        print ''
-        exit()
-
-    return s
-
 
 #-----------------------------------------------------------------------------
 def uuid1mc_insecure():
@@ -325,7 +282,8 @@ class FileProducer(object):
             traceback.print_exception(exc_type, exc_value, exc_traceback, limit=2, file=sys.stdout)
             for key in value:
                 print "   {}: ".format(key),
-                print to_unicode_or_bust(value[key], True)
+        finally:
+            return
         
 
     def save(self, info):
@@ -399,91 +357,6 @@ class ScanShareFiles:
         self.dir_count = 0
 
 
-    def change(self, root, file):
-        file2 = file
-        try:
-            file2 = file.decode('cp1252').encode('utf8', 'xmlcharrefreplace')
-        finally:
-            if file2 != file:
-                print ('rename: {} | {}, {}'.format(root,file,file2))
-                file = os.path.join(root, file)
-                file2 = os.path.join(root, file2)
-#                os.renames(file, file2)
-
-
-    def convert(self, basedir):
-        for root, dirs, files in os.walk(basedir):
-           for name in files:
-               self.change(root, name)
-               self.file_count += 1
-           for name in dirs:
-               self.dir_count += 1
-               self.change(root, name)
-        print ('   detected {} files on {}'.format(self.file_count, basedir))
-
-
-    def fixDirs(self, basedir):
-        for root, dirs, files in os.walk(basedir):
-            for name in files:
-                if is_not_ascii(name):
-                   to_unicode_or_bust(name),
-                   print ('   {}:  {} -> '.format(root,name))
-                self.file_count += 1
-            for name in dirs:
-                if is_not_ascii(name):
-                   to_unicode_or_bust(name),
-                   print ('   {}:  {} -> '.format(root,name))
-                self.dir_count += 1
-
-
-    def getDirType(self, dir, files):
-        if len(files)>0:
-            with tempfile.NamedTemporaryFile(mode='w+t',delete=False) as fh:
-                for line in files:
-                    fh.write(line)
-                fh.close()
-                stream = os.popen('file '+fh.name)
-                info = stream.read()
-                stream.close()
-                os.unlink(fh.name)
-                return info
-        return None
-
-
-    def listDirs(self, basedir):
-        self.producer = FileProducer('utf8.files')
-        filelist = []
-        lastroot = None
-        for root, dirs, files in os.walk(basedir):
-            for name in files:
-                if lastroot != root:
-                    self.saveDirType(lastroot, filelist)
-                    filelist = []
-                    lastroot = root
-                self.file_count += 1
-                filelist.append(name + "\n")
-            for name in dirs:
-                self.dir_count += 1
-        self.saveDirType(lastroot, filelist)
-        self.producer.close()
-        print ('   detected {} files on {}'.format(self.file_count, basedir))
-
-
-    def listFiles(self, basedir):
-        fh = tempfile.NamedTemporaryFile(mode='w+t', delete=False)
-        for root, dirs, files in os.walk(basedir):
-            for name in files:
-                self.file_count += 1
-                file = os.path.join(root, name).decode('cp1252')
-                fh.write(file.encode('utf8', 'xmlcharrefreplace') + "\n")
-            for name in dirs:
-                self.dir_count += 1
-        fh.flush()
-        fh.close()
-        print ('   detected {} files on {}'.format(self.file_count, basedir))
-        return fh.name
-
-
     def main(self, cmdargs):
         args = GetArgs()
         args.validate_options()
@@ -496,8 +369,9 @@ class ScanShareFiles:
         else:
             self.producer = FileProducer(args.file, args.topic)
 
-#        args.dirs = ['/mnt/Synology/Guest/All Users/Music.todo/20100702/Music/Wendy Carlos/Switched On Bach II',
-#                     '/mnt/Synology/Guest/All Users/Music.todo/20100619.Music.org/Faust/Faust IV']
+#        args.dirs = ['/mnt/Synology/Guest/All Users/Music.todo/20100822/Music.Partial/Tool']
+
+        self.data = dict()
 
         for dir in args.dirs:
             print('dirs:  '+dir)
@@ -512,64 +386,38 @@ class ScanShareFiles:
 
 
     def produceData(self, basedir):
-        shas = dict()
-        data = dict()
-        for root, dirs, files in os.walk(basedir):
-            sha = hashlib.sha256()
-            for name in files:
-                self.file_count += 1
-                fileInfo(name, root, data)
-                sha.update(data['sha256'])
-                self.producer.produce(data)
-            for name in dirs:            
-                self.dir_count += 1
-                sha.update(data['sha256'])
-                dirInfo(name, root, shas)
-                self.producer.produce(data)
-            shas[root] = sha.hexdigest()
-        print ('   detected {} files on {}'.format(self.file_count, basedir))
+        file_count = 0
+        dir_count = 0
+        sha = hashlib.sha256()
+        size = 0
+
+        for name in os.listdir(basedir):
+            file = os.path.join(basedir, name)
+
+            if os.path.isdir(file):
+                dir_count += 1
+                self.produceData(file)
+
+            else:
+                file_count += 1
+                fileInfo(file, self.data)
+                self.producer.produce(self.data)
+
+            if 'size' in self.data.keys():
+                size += self.data['size']
+
+            if 'sha256' in self.data.keys():
+                sha.update(self.data['sha256'])
 
 
-    def saveDirType(self, dir, files):
-        info = self.getDirType(dir, files)
-        if info:
-            matches = re.match(r'^.+:\s+(ISO-8859 text).*$', info, re.M)
-            if matches:
-                self.producer.save(dir)
-            
+        fileInfo(basedir, self.data)
+        self.data['size'] = size
+        self.data['file_count'] = file_count
+        self.data['dir_count'] = dir_count
+        self.data['sha256'] = sha.hexdigest()
+        self.producer.produce(self.data)
 
-
-    def showDirs(self, basedir):
-        filelist = []
-        lastroot = None
-        for root, dirs, files in os.walk(basedir):
-            for name in files:
-                if lastroot != root:
-                    self.getDirType(lastroot, filelist)
-                    filelist = []
-                    lastroot = root
-                self.file_count += 1
-                filelist.append(name + "\n")
-            for name in dirs:
-                self.dir_count += 1
-        self.getDirType(lastroot, filelist)
-        print ('   detected {} files on {}'.format(self.file_count, basedir))
-
-
-    def testNames(self, basedir):
-        fh = open('filenames_utf.txt', mode='w+t')
-        for root, dirs, files in os.walk(basedir):
-            for name in files:
-                self.file_count += 1
-#                if not isinstance(name, unicode):
-#                    fh.write(root + '/' + name + "\n")
-            for name in dirs:
-                self.dir_count += 1
-                if not isinstance(name, unicode):
-                    fh.write(root + '/' + name + "\n")
-        fh.flush()
-        fh.close()
-        print ('   detected {} files on {}'.format(self.file_count, basedir))
+        print ('   detected {} dirs and {} files on {}'.format(dir_count, file_count, basedir))
 
 
 #-----------------------------------------------------------------------------
